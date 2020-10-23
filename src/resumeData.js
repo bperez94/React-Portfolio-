4ef3a39024d1249afe6496ab94cfb964633a5eb1
Brandon Perez <brandonp940@gmail.com>
2020-10-23 00:09:27 -0400

    will render info but needs styling and formatting

---
+++ src/resumeData.js
@@ -0,0 +1,74 @@
+let resumeData = {
+
+'imagebaseurl':'https://github.com/bperez94',
+'name': 'Brandon Perez',
+'role': 'Full Satck and Back EndDeveloper',
+'linkedinId':'https://www.linkedin.com/in/brandon-perez-6460b81b1/',
+'roleDescription': 'I like to learn and create applications in various parts of software development and like to learn about new technologies, write technical articles or simply play games in my free time.',
+'socialLinks':[
+{
+'name':'linkedin',
+'url':'https://www.linkedin.com/in/brandon-perez-6460b81b1/',
+},
+{
+'name':'github',
+'url':'https://github.com/bperez94',
+},
+],
+'aboutme':'I am a Full Stack and Back End developer. Currently studying to obtained my Certificate for completing the rigours coding and web design course at George Washington University in Arlington, Virginia. Due to my self driven nature and high level of motivation I was able to balance being an essential worker at whole foods and academic studies. Being versed in HTML,CSS,JAVASCRIPT, BULMA, and JQUERY I was able to play a pivotal role in the development of an online game tournament tracker. The team I worked on built it to track multiple games across different platforms. I’m eager to use my ingenuity and dexterity to provide unique user experiences',
+'address':'Fredricksbrug,V.A.',
+'education':[
+{
+'UniversityName':'George Washignton',
+'specialization':'Full Stack Web Development',
+'MonthOfPassing':'October',
+'YearOfPassing':'2020',
+},
+],
+'skillsDescription':'I am versed in front and back end devlopment in test driven environments.',
+'skills':[
+{
+'skillname':'HTML'
+},
+{
+'skillname':'CSS'
+},
+{
+'skillname':'Reactjs'
+}
+],
+'portfolio':[
+{
+'name':'Esports Tournament tracker',
+'description':'Select Difficulty is a mobile-friendly website tailored to both avid eSports fans as well as beginner fans who are open to being introduced to the world of competitive video gaming. Beginners can come to learn by watching gaming personalities, and be immersed in the competitive nature of major tournaments. This is done with regular website maintenance, and the integration of the Twitch and PandaScore APIs',
+'imgurl':'#',
+'url':'https://wheaties5588.github.io/esports-tournament-tracker/'
+},
+// {
+// “name”:”project2",
+// “description”:”mobileapp”,
+// “imgurl”:”images/portfolio/project.jpg”
+// },
+// {
+// “name”:”project3",
+// “description”:”mobileapp”,
+// “imgurl”:”images/portfolio/project2.png”
+// },
+// {
+// “name”:”project4",
+// “description”:”mobileapp”,
+// “imgurl”:”images/portfolio/phone.jpg”
+// }
+// ],
+// “testimonials”:[
+// {
+// “description”:”This is a sample testimonial”,
+// “name”:”Some technical guy”
+// },
+// {
+// “description”:”This is a sample testimonial”,
+// “name”:”Some technical guy”
+// }
+ ]
+ }
+export default resumeData
